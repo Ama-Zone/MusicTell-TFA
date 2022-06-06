5cac26e067fcdbbc7950baeb2816ed23f3e859a0
Ama-Zone <100293017+Ama-Zone@users.noreply.github.com>
2022-06-06 19:48:35 +0200

    Test01

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+console.info('Hello world');
+
+import { gsap } from "gsap";
+console.log(gsap.version);
+
+/*gsap.to('.box',{ x:100, rotation: 360, backgroundColor: 'green', duration: 2});*/
+
+
+/*gsap.fromto('.box',{ x: -100, opacity: 0}, {x: 100, duration: 2, opacity :1});*/
+
+gsap.to('.box',{ du});
