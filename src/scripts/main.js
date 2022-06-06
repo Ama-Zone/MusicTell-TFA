9c70c967ea13f2b7972750e4555a1f0fc5e5cb7f
Ama-Zone <100293017+Ama-Zone@users.noreply.github.com>
2022-06-06 20:08:18 +0200

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,58 @@
-console.info('Hello world');
+
 
 import { gsap } from "gsap";
 console.log(gsap.version);
 
 /*gsap.to('.box',{ x:100, rotation: 360, backgroundColor: 'green', duration: 2});*/
-
-
 /*gsap.fromto('.box',{ x: -100, opacity: 0}, {x: 100, duration: 2, opacity :1});*/
 
-gsap.to('.box',{ du});
+
+/*dark Mode*/
+let activeTheme = localStorage.getItem("theme");
+if(activeTheme != null){
+   darklight(activeTheme);
+}
+let themeTrigger = document.querySelector(".js")
+if(themeTrigger){
+    themeTrigger.addEventListener("click", choix);
+}
+
+function darklight(theme){
+    let imdButton = document.querySelector(".darkmode");
+    let imdDwm = document.querySelector(".footer__info--logodwm");
+    let imdlogo = document.querySelector(".logo");
+
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+    imdButton.setAttribute("data-theme", theme);
+    imdDwm.setAttribute("data-theme", theme);
+    imdlogo.setAttribute("data-theme", theme);
+
+    if (theme == "dark"){
+        imdButton.setAttribute('src','assets/images/lune-DOP.svg');
+        imdDwm.setAttribute('src','assets/images/logo-dwt_light.svg');
+        imdlogo.setAttribute('src','assets/images/logo_DOP.svg');
+    }else if (theme == "light"){
+        imdButton.setAttribute('src','assets/images/soleil-BOP.svg');
+        imdDwm.setAttribute('src','assets/images/logo-dwt.svg');
+        imdlogo.setAttribute('src','assets/images/logo_BOP.svg');
+    }
+}
+
+function choix(){
+
+    let theme = document.body.getAttribute("data-theme");
+    
+    if(theme == "dark"){
+        darklight("light");
+    }else{
+        darklight("dark");
+    }
+}
+
+
+
+/*date*/
+
+let date = new Date().getFullYear();
+document.getElementById("annee").innerHTML = date;
